03ae86ffeb2d30f9c54fe4195bd2d9830fc47ad1
Cesar Ochoa <cesar.ochoa@bizagi.com>
2018-06-13 13:21:43 -0500

    Store the numbers in a hash table

--- cesarartefactid/src/main/java/cesargroupid/cesarartefactid/Calculator.java
+++ cesarartefactid/src/main/java/cesargroupid/cesarartefactid/Calculator.java
@@ -1,7 +1,36 @@
 package cesargroupid.cesarartefactid;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
+
 public class Calculator {
 	
+	public Hashtable<String,String> hastablePrimes = new Hashtable<String,String>();
+
+	public boolean searchPrimeNumberOne(int number) {
+		int countDivisor = 0;
+		for (int i = 1; i <= number; i++) {
+			if (number % i == 0) {
+				countDivisor++;
+			}
+		}
+		if (countDivisor > 2)
+			return false;
+		else {
+			return true;
+		}
+
+	}
 	
+	public void storeListCousinsHash(int number) {		
+		hastablePrimes.put(" "+hastablePrimes.size(), " "+number);		
+	}
+	
+	public void showPrimesInHash(Hashtable<String,String> hastable) {
+		Enumeration<String> enumeration = hastable.elements();
+		while (enumeration.hasMoreElements()) {
+		  System.out.println("Numero: " + enumeration.nextElement());
+		}
+	}
 
 }
--- cesarartefactid/src/main/java/cesargroupid/cesarartefactid/PrimeCalculation.java
+++ cesarartefactid/src/main/java/cesargroupid/cesarartefactid/PrimeCalculation.java
@@ -8,6 +8,15 @@ public class PrimeCalculation
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Calculator miCalculator= new Calculator();
+        
+        for (int i = 0; i < 100; i++) {
+        	
+			if (miCalculator.searchPrimeNumberOne(i)) {
+				miCalculator.storeListCousinsHash(i);
+				
+			}
+		}
+        miCalculator.showPrimesInHash(miCalculator.hastablePrimes);
     }
 }
