72c54d103ba54cf603f525562d66180447633387
Cesar Ochoa <cesar.ochoa@bizagi.com>
2018-06-13 19:00:08 -0500

    execution with 1000000 numbers, 13 mins

--- cesarartefactid/src/main/java/cesargroupid/cesarartefactid/PrimeCalculation.java
+++ cesarartefactid/src/main/java/cesargroupid/cesarartefactid/PrimeCalculation.java
@@ -9,14 +9,17 @@ public class PrimeCalculation
     public static void main( String[] args )
     {
         Calculator miCalculator= new Calculator();
+        long initTime = System.nanoTime(); 
         
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000000; i++) {
         	
 			if (miCalculator.searchPrimeNumberOne(i)) {
 				miCalculator.storeListCousinsHash(i);
 				
 			}
 		}
+        long durationTime = ((System.nanoTime() - initTime)/1000000)/1000;        
         miCalculator.showPrimesInHash(miCalculator.hastablePrimes);
+        System.err.println(durationTime);
     }
 }
