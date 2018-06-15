8a6efeffb8de2a18589a67446b7e567d76321cc1
Cesar Ochoa <cesar.ochoa@bizagi.com>
2018-06-15 17:44:28 -0500

    Time in Millis

--- cesarartefactid/src/main/java/cesargroupid/cesarartefactid/PrimeCalculation.java
+++ cesarartefactid/src/main/java/cesargroupid/cesarartefactid/PrimeCalculation.java
@@ -10,8 +10,9 @@ public class PrimeCalculation
     {
         Calculator miCalculator= new Calculator();
         long initTime = System.nanoTime(); 
+        long initTimeMill = System.currentTimeMillis(); 
         
-        for (int i = 0; i < 250; i++) {
+        for (int i = 0; i < 1000000; i++) {
         	
 			if (miCalculator.searchPrimeNumberTwo(i)) {
 				//miCalculator.storeListCousinsHash(i);
@@ -21,9 +22,11 @@ public class PrimeCalculation
 		}
         
         long durationTime = (((System.nanoTime() - initTime)/1000)/1000)/1000; 
+        long durationTimeMill= (System.currentTimeMillis() - initTimeMill)/1000;
         //System.err.println(durationTime);
         //miCalculator.showPrimesInHash(miCalculator.hastablePrimes);
         miCalculator.showPrimesInArray(miCalculator.arrayPrimes); 
         System.err.println(durationTime);
+        System.err.println(durationTimeMill);
     }
 }
